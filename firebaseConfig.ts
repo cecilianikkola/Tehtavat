b2dea9e3ef463c5a93c2c2ade603eadfed2fc921
cecilianikkola <148796507+cecilianikkola@users.noreply.github.com>
2023-11-13 18:23:55 +0200

    Update firebaseConfig.ts

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,8 +1,8 @@
 export default {
-    apiKey: "xyz!#¤%&/",
-    authDomain: "pilvi-react.firebaseapp.com",
-    projectId: "pilvi-react",
-    storageBucket: "pilvi-react.appspot.com",
+    apiKey: "AIzaSyAqnkAymJysehfpypZf7CV_vgXJF90dSnU",
+    authDomain: "pilvi-vite.firebaseapp.com",
+    projectId: "pilvi-vite-1b8b7",
+    storageBucket: "pilvi-vite.appspot.com",
     messagingSenderId: "123456",
-    appId: "1:123456:web:789abcdef",
+    appId: "1:618924819874:web:bac27a4ff292c0e7385efb",
   };
