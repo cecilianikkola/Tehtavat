7bf0f1406f8abbcda4b7609ef2908e5e76e57c0d
cecilianikkola <148796507+cecilianikkola@users.noreply.github.com>
2023-11-13 18:08:54 +0200

    Create firebaseConfig.ts

---
+++ firebaseConfig.ts
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "xyz!#¤%&/",
+    authDomain: "pilvi-react.firebaseapp.com",
+    projectId: "pilvi-react",
+    storageBucket: "pilvi-react.appspot.com",
+    messagingSenderId: "123456",
+    appId: "1:123456:web:789abcdef",
+  };
